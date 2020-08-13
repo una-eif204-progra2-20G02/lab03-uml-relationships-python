8fdc028a8309be1bd98179557a5b2fd646157164
Diana Fallas <65523878+dianattrix@users.noreply.github.com>
2020-08-12 19:02:38 -0600

    Update

--- Student.cpp
+++ Student.cpp
@@ -2,3 +2,28 @@
 // Created by diana on 12/8/2020.
 //
 
+#include "Student.h"
+
+Student::Student() {
+    carnet = "";
+}
+
+Student::Student(string &name, string &id, string carnet) : Person(name, id){
+    this->carnet = carnet;
+}
+
+void Student::setCarnet(string carnet) {
+this->carnet = carnet;
+}
+
+string Student::getCarnet() {
+    return carnet;
+}
+
+string Student::toString() {
+    stringstream s;
+    s << Person::toString();
+    s << "Carnet: " << carnet << endl;
+    return s.str();
+}
+
--- Student.h
+++ Student.h
@@ -4,7 +4,23 @@
 
 #ifndef BASIC_00_EXAMPLE_STUDENT_H
 #define BASIC_00_EXAMPLE_STUDENT_H
+#include<iostream>
+#include<sstream>
+using namespace std;
+#include"Person.h"
+class Student : public Person
+{
+private:
+    string carnet;
 
+public:
+    Student();
+    Student(string& name, string& id, string carnet);
+    void setCarnet(string carnet);
+    string getCarnet();
+    string toString();
+
+};
 
 
 
