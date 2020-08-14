2c387679a6817bffe6c44fbc526bbbc11883feb1
Diana Fallas <65523878+dianattrix@users.noreply.github.com>
2020-08-14 09:18:35 -0600

    Create University.h

---
+++ University.h
@@ -0,0 +1,13 @@
+//
+// Created by diana on 14/8/2020.
+//
+
+#ifndef LAB03_UML_RELATIONSHIPS_PYTHON_UNIVERSITY_H
+#define LAB03_UML_RELATIONSHIPS_PYTHON_UNIVERSITY_H
+
+
+class University
+{
+    
+};
+#endif //LAB03_UML_RELATIONSHIPS_PYTHON_UNIVERSITY_H
